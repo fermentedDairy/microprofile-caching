388056467458ae84742264a0aa7a0f990a5aeeee
edwardc <edward.cheesman@gmail.com>
2023-05-30 07:37:40 +0200

    Adding some cache hit test cases

--- microprofile-caching-api/src/test/java/org/fermented/dairy/microprofile/caching/interceptors/CachingRetrieveInterceptorTest.java
+++ microprofile-caching-api/src/test/java/org/fermented/dairy/microprofile/caching/interceptors/CachingRetrieveInterceptorTest.java
@@ -3,6 +3,7 @@ package org.fermented.dairy.microprofile.caching.interceptors;
 import jakarta.interceptor.InvocationContext;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.fermented.dairy.microprofile.caching.interfaces.CacheProvider;
+import org.fermented.dairy.microprofile.caching.test.entities.CacheEntityWithProvider;
 import org.fermented.dairy.microprofile.caching.test.entities.CachingClass;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +21,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -45,14 +47,14 @@ class CachingRetrieveInterceptorTest {
         FieldUtils.writeField(cachingRetrieveInterceptor, "defaultTTL", 300000L, true);
     }
 
-    @DisplayName("given a key that is not in the cache then insert non null result into cache")
+    @DisplayName("(Cache Miss) given a key that is not in the cache then insert non null result into cache")
     @Test
     void givenAKeyThatIsNotInTheCacheThenInsertNonNullResultIntoCache() throws Exception {
         Method cachingMethod = Arrays.stream(CachingClass.class.getDeclaredMethods()).filter(method -> method.getName().equals("getCachedSingleParam")).findFirst().get();
         when(invocationContext.getMethod()).thenReturn(cachingMethod);
         when(invocationContext.getParameters()).thenReturn(new Object[]{1L});
 
-        when(cacheProviderMap.get("TestCacheProvider").getFromCache(1L, "", CachingClass.class)).thenReturn(Optional.empty());
+        when(cacheProviderMap.get("TestCacheProvider").getFromCache(1L, "TestCacheName", CacheEntityWithProvider.class)).thenReturn(Optional.empty());
         when(invocationContext.proceed()).thenReturn(cachingClass.getCachedSingleParam(1L));
         Object result = cachingRetrieveInterceptor.doCacheRetrieve(invocationContext);
 
@@ -60,19 +62,62 @@ class CachingRetrieveInterceptorTest {
 
     }
 
-    @DisplayName("given a key that is not in the cache and a fetch method with an optional return type then insert non null result into cache")
+    @DisplayName("(Cache Miss) given a key that is not in the cache and a fetch method with an optional return type then insert non null result into cache")
     @Test
     void givenAKeyThatIsNotInTheCacheAndAFetchMethodWithAnOptionalReturnTypeThenInsertNonNullResultIntoCache() throws Exception {
         Method cachingMethod = Arrays.stream(CachingClass.class.getDeclaredMethods()).filter(method -> method.getName().equals("getOptionalCachedSingleParam")).findFirst().get();
         when(invocationContext.getMethod()).thenReturn(cachingMethod);
         when(invocationContext.getParameters()).thenReturn(new Object[]{1L});
 
-        when(cacheProviderMap.get("TestCacheProvider").getFromCache(1L, "", CachingClass.class)).thenReturn(Optional.empty());
+        when(cacheProviderMap.get("TestCacheProvider").getFromCache(1L, "TestCacheName", CacheEntityWithProvider.class)).thenReturn(Optional.empty());
         when(invocationContext.proceed()).thenReturn(cachingClass.getOptionalCachedSingleParam(1L));
         Object optionalResult = cachingRetrieveInterceptor.doCacheRetrieve(invocationContext);
 
         assertTrue(((Optional)optionalResult).isPresent());
         verify(cacheProviderMap.get("TestCacheProvider")).putIntoCache(1L, ((Optional)optionalResult).get(), "TestCacheName", 300000L);
+
+    }
+
+    @DisplayName("(Cache Hit) given a key that is in the cache and a fetch method with an optional return type then return cached result")
+    @Test
+    void givenAKeyThatIsInTheCacheAndAFetchMethodWithAnOptionalReturnTypeThenReturnCachedResult() throws Exception {
+        Method cachingMethod = Arrays.stream(CachingClass.class.getDeclaredMethods()).filter(method -> method.getName().equals("getOptionalCachedSingleParam")).findFirst().get();
+        when(invocationContext.getMethod()).thenReturn(cachingMethod);
+        when(invocationContext.getParameters()).thenReturn(new Object[]{1L});
+
+        when(cacheProviderMap.get("TestCacheProvider").getFromCache(1L, "TestCacheName", CacheEntityWithProvider.class)).thenReturn(Optional.of(
+                CacheEntityWithProvider.builder()
+                        .id(1L)
+                        .name("firstName")
+                        .surname("surname")
+                        .build()
+        ));
+
+        Object optionalResult = cachingRetrieveInterceptor.doCacheRetrieve(invocationContext);
+
+        assertTrue(((Optional)optionalResult).isPresent());
+        verify(cacheProviderMap.get("TestCacheProvider"), never()).putIntoCache(1L, ((Optional)optionalResult).get(), "TestCacheName", 300000L);
+
+    }
+
+    @DisplayName("(Cache Hit) given a key that is in the cache and a fetch method with return type then return cached result")
+    @Test
+    void givenAKeyThatIsInTheCacheAndAFetchMethodWithAReturnTypeThenReturnCachedResult() throws Exception {
+        Method cachingMethod = Arrays.stream(CachingClass.class.getDeclaredMethods()).filter(method -> method.getName().equals("getOptionalCachedSingleParam")).findFirst().get();
+        when(invocationContext.getMethod()).thenReturn(cachingMethod);
+        when(invocationContext.getParameters()).thenReturn(new Object[]{1L});
+
+        when(cacheProviderMap.get("TestCacheProvider").getFromCache(1L, "TestCacheName", CacheEntityWithProvider.class)).thenReturn(Optional.of(
+                CacheEntityWithProvider.builder()
+                        .id(1L)
+                        .name("firstName")
+                        .surname("surname")
+                        .build()
+        ));
+
+        Object result = cachingRetrieveInterceptor.doCacheRetrieve(invocationContext);
+
+        verify(cacheProviderMap.get("TestCacheProvider"), never()).putIntoCache(1L, result, "TestCacheName", 300000L);
 
     }
 }
